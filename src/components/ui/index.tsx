a54d7f046a7122047c681318b0e2cc7e6a9a496b
Felipe Souza <j.felipe.dev.18@gmail.com>
2024-02-21 03:36:37 -0300

    delete mountain icon and create central import in components/ui

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,19 @@
+import { Input } from './input'
+import { Button, ButtonProps } from './button'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from './accordion'
+
+export {
+  Input,
+  Button,
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+}
+
+export type { ButtonProps }
